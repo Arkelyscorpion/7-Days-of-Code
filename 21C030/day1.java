06dcbab76db0d6408d5445d6ca095647be2780e4
B GOKUL <gokul0880@gmail.com>
2023-09-05 19:07:48 +0530

    Day1-21C030

--- 21C030/day1.java
+++ 21C030/day1.java
@@ -4,13 +4,13 @@ public final class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int bacteriaAdded = 0;
+        int finalbac = 0;
 
         while (n > 0) {
-            bacteriaAdded += n % 2;
+            finalbac += n % 2;
             n /= 2;
         }
 
-        System.out.println(bacteriaAdded);
+        System.out.println(finalbac);
     }
 }
