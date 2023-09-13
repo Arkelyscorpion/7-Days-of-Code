568a4922e0a67185a8eb59bd23886e43b9bac3e7
Prathima <prathimapariharan@gmail.com>
2023-09-14 00:05:30 +0530

    21C071 - day6 and day7

--- 21C071/day6.java
+++ 21C071/day6.java
@@ -3,20 +3,16 @@ public final class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
-        int max=0,temp=0;
         for(int i=0; i<t; i++){
             long l=sc.nextLong();
             long r=sc.nextLong();
             long a=sc.nextLong();
-            for(long j=l; j<=r;j++){
-                temp= (int) (Math.floor(j/a)+(j%a));
-                if(temp>max){
-                    max=temp;
-                }
+            if(l/a == r/a){
+                System.out.println(r/a+r%a);
             }
-            System.out.println(max);
-            max=0;
-            temp=0;
+            else{
+                System.out.println(Math.max(r/a+ r%a, r/a -1 + a-1));
+            }
         }
     }
 }
